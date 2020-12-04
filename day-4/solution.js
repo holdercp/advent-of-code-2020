a3812c9d2db8db96052ee360b3fecced42dc1022
holdercp <chrispholder@gmail.com>
2020-12-04 00:37:07 -0600

    Complete day 4, part 1

--- day-4/solution.js
+++ day-4/solution.js
@@ -6,10 +6,26 @@ const {
 } = require("../helpers/readAndTransformInputFile");
 
 const inputFilePath = path.resolve(__dirname, "./input.txt");
-const data = readAndTransformInputFile(inputFilePath);
+
+const buildPassportObjectsReducer = (obj, field) => {
+  const [key, value] = field.split(":");
+  return Object.assign({}, obj, { [key]: value });
+};
+
+const passportData = readAndTransformInputFile(
+  inputFilePath,
+  "\n\n"
+).map((passport) =>
+  passport.split(/\s/).reduce(buildPassportObjectsReducer, {})
+);
+
+const REQUIRED_FIELDS = ["hgt", "hcl", "pid", "ecl", "eyr", "iyr", "byr"];
+
+const hasRequiredFieldsFilter = (passport) =>
+  REQUIRED_FIELDS.every((field) => Object.keys(passport).includes(field));
 
 function getSolution(part = "1") {
-  return data;
+  return passportData.filter(hasRequiredFieldsFilter).length;
 }
 
 module.exports = { getSolution };
