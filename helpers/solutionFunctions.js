491ea74567198792d88a9ae102437cc24c1b2e76
holdercp <chrispholder@gmail.com>
2020-12-04 01:37:07 -0600

    Scaffold day 5

--- helpers/solutionFunctions.js
+++ helpers/solutionFunctions.js
@@ -4,12 +4,14 @@ const day1 = require("../day-1/solution.js");
 const day2 = require("../day-2/solution.js");
 const day3 = require("../day-3/solution.js");
 const day4 = require("../day-4/solution.js");
+const day5 = require("../day-5/solution.js");
 
 const solutionFunctions = {
   day1: day1.getSolution,
   day2: day2.getSolution,
   day3: day3.getSolution,
   day4: day4.getSolution,
+  day5: day5.getSolution,
 };
 
 module.exports = solutionFunctions;
