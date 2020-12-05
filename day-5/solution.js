8d7a65c455fac59e594d1fea6b3138d7ca0acaac
holdercp <chrispholder@gmail.com>
2020-12-05 00:58:45 -0600

    Complete day 5, part 1

--- day-5/solution.js
+++ day-5/solution.js
@@ -7,10 +7,45 @@ const {
 
 const inputFilePath = path.resolve(__dirname, "./input.txt");
 
-const data = readAndTransformInputFile(inputFilePath);
+const passes = readAndTransformInputFile(inputFilePath);
+
+const ROW_ZONE = [...Array(128).keys()];
+const COL_ZONE = [...Array(8).keys()];
+const DIVIDER_MAPPING = {
+  F: "lower",
+  B: "upper",
+  L: "lower",
+  R: "upper",
+};
+
+const splitAtIndex = (str, index) => [str.slice(0, index), str.slice(index)];
+
+const halveZone = (zone, divider) => {
+  const mid = Math.round((zone.length - 1) / 2);
+  if (zone.length === 2) {
+    return divider === "lower" ? zone[0] : zone[1];
+  }
+
+  return divider === "lower" ? zone.slice(0, mid) : zone.slice(mid);
+};
+
+const zoneReducer = (zone, divider) =>
+  halveZone(zone, DIVIDER_MAPPING[divider]);
+
+const getSeatNumber = (pass) => {
+  const [rowDividers, colDividers] = splitAtIndex(pass, 7);
+  const rowNumber = rowDividers.split("").reduce(zoneReducer, ROW_ZONE);
+  const colNumber = colDividers.split("").reduce(zoneReducer, COL_ZONE);
+  return [rowNumber, colNumber];
+};
+
+const getSeatId = (pass) => {
+  const [row, col] = getSeatNumber(pass);
+  return row * 8 + col;
+};
 
 function getSolution(part = "1") {
-  return data;
+  return passes.map(getSeatId).sort((a, b) => b - a)[0];
 }
 
 module.exports = { getSolution };
