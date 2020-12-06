d216d29fa9296a6e5f916cc3beeb7f9bc1312a39
holdercp <chrispholder@gmail.com>
2020-12-06 00:06:40 -0600

    Complete day 6

--- day-6/solution.js
+++ day-6/solution.js
@@ -7,10 +7,43 @@ const {
 
 const inputFilePath = path.resolve(__dirname, "./input.txt");
 
-const data = readAndTransformInputFile(inputFilePath);
+const groups = readAndTransformInputFile(inputFilePath, "\n\n").map((group) =>
+  group.trim().split("\n")
+);
+
+const onlyUniqueFilter = (value, index, arr) => arr.indexOf(value) === index;
+
+const getUniqueAnswers = (answers) => {
+  const allAnswers = answers.reduce(
+    (answerStr, answer) => answerStr + answer,
+    ""
+  );
+  return allAnswers.split("").filter(onlyUniqueFilter);
+};
+
+const getAnswerCounts = (answers) =>
+  answers.reduce((acc, answer) => {
+    answer.split("").forEach((element) => {
+      acc[element] = acc[element] ? acc[element] + 1 : 1;
+    });
+
+    return acc;
+  }, {});
+
+const uniqueAnswersSum = (sum, group) => sum + getUniqueAnswers(group).length;
+
+const sameAnswersSum = (sum, group) => {
+  const answerCounts = getAnswerCounts(group);
+  const numOfSameAnswers = Object.values(answerCounts).filter(
+    (count) => count === group.length
+  ).length;
+
+  return sum + numOfSameAnswers;
+};
 
 function getSolution(part = "1") {
-  return data;
+  const reducer = part === "2" ? sameAnswersSum : uniqueAnswersSum;
+  return groups.reduce(reducer, 0);
 }
 
 module.exports = { getSolution };
