9733adf8a3613898ba6c2d8b0418fb6525d53548
holdercp <chrispholder@gmail.com>
2020-12-07 22:39:50 -0600

    Complete day 7

--- day-7/solution.js
+++ day-7/solution.js
@@ -7,10 +7,73 @@ const {
 
 const inputFilePath = path.resolve(__dirname, "./input.txt");
 
-const data = readAndTransformInputFile(inputFilePath);
+const normalizeBagColor = (color) =>
+  color
+    .split(" bag")[0]
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+
+const innerBagReducer = (data, bag) => {
+  const separatorIndex = bag.indexOf(" ");
+  const count = bag.slice(0, separatorIndex);
+  if (count === "no") {
+    return null;
+  }
+
+  const color = bag.slice(separatorIndex + 1);
+  const normalizedColor = normalizeBagColor(color);
+  return Object.assign({}, data, { [normalizedColor]: parseInt(count, 10) });
+};
+
+const bagRuleReducer = (data, rule) => {
+  const [color, contents] = rule.split(" contain ");
+  const normalizedColor = normalizeBagColor(color);
+  const innerBags = contents.split(", ");
+  const bagContents = innerBags.reduce(innerBagReducer, {});
+
+  return Object.assign({}, data, { [normalizedColor]: bagContents });
+};
+
+const bagRules = readAndTransformInputFile(inputFilePath).reduce(
+  bagRuleReducer,
+  {}
+);
+
+const searchBag = (color, searchColor) => {
+  if (color === searchColor) {
+    return true;
+  }
+  if (bagRules[color] === null) {
+    return false;
+  }
+
+  return Object.keys(bagRules[color]).some((innerColor) =>
+    searchBag(innerColor, searchColor)
+  );
+};
+
+const searchBagFilter = (color) => {
+  if (!color || color === "shinyGold") {
+    return false;
+  }
+  return searchBag(color, "shinyGold");
+};
+
+const getInnerBagCount = (color) => {
+  if (bagRules[color] === null) {
+    return 0;
+  }
+  return Object.entries(bagRules[color]).reduce((count, innerColor) => {
+    return count + innerColor[1] * (1 + getInnerBagCount(innerColor[0]));
+  }, 0);
+};
 
 function getSolution(part = "1") {
-  return data;
+  if (part === "2") {
+    return getInnerBagCount("shinyGold");
+  }
+
+  return Object.keys(bagRules).filter(searchBagFilter).length;
 }
 
 module.exports = { getSolution };
