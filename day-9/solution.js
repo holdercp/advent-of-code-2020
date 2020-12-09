417fc8c8136401c6c83544a7bf7eb66f71f20c3f
holdercp <chrispholder@gmail.com>
2020-12-09 02:06:35 -0600

    Complete day 9 brute force because it's 2am

--- day-9/solution.js
+++ day-9/solution.js
@@ -6,14 +6,77 @@ const {
 } = require("../helpers/readAndTransformInputFile");
 
 const inputFilePath = path.resolve(__dirname, "./input.txt");
-const data = readAndTransformInputFile(inputFilePath);
+const data = readAndTransformInputFile(inputFilePath).map((digit) =>
+  parseInt(digit, 10)
+);
+
+const numIsValid = (addends, sum) => {
+  for (let i = 0; i < addends.length - 1; i += 1) {
+    const addend1 = addends[i];
+    for (let j = i + 1; j < addends.length; j += 1) {
+      const addend2 = addends[j];
+      if (addend1 + addend2 === sum) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+const sumReducer = (sum, addend) => sum + addend;
+
+const findAddends = (addends, targetSum) => {
+  let addendsToSum = [];
+  let currentIndex = 0;
+  do {
+    const sum = addendsToSum.reduce(sumReducer, 0);
+    if (sum === targetSum) {
+      return addendsToSum;
+    }
+
+    if (currentIndex < addends.length) {
+      const currentAddend = addends[currentIndex];
+      if (sum + currentAddend > targetSum) {
+        addendsToSum.shift();
+      }
+
+      addendsToSum.push(currentAddend);
+    } else {
+      addendsToSum.shift();
+    }
+
+    currentIndex += 1;
+  } while (addendsToSum.length > 0);
+
+  return addendsToSum;
+};
 
 function part1() {
-  return "NOT_IMPLEMENTED";
+  const preambleLength = 25;
+  const rest = data.slice(preambleLength);
+
+  for (let i = 0; i < rest.length; i += 1) {
+    const addends = data.slice(i, preambleLength + i);
+
+    if (!numIsValid(addends, rest[i])) {
+      return rest[i];
+    }
+  }
+
+  return null;
 }
 
 function part2() {
-  return "NOT_IMPLEMENTED";
+  const invalidSum = part1();
+  for (let i = 0; i < data.length; i += 1) {
+    const validAddends = findAddends(data.slice(i), invalidSum);
+    if (validAddends.length > 0) {
+      validAddends.sort((a, b) => a - b);
+      return validAddends[0] + validAddends[validAddends.length - 1];
+    }
+  }
+
+  return "I hate my life";
 }
 
 module.exports = { part1, part2 };
