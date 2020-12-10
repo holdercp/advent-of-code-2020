9c2699a1cc173684fe9f3e2cfb6cbfbbf6966622
holdercp <chrispholder@gmail.com>
2020-12-10 11:06:07 -0600

    Complete day 10... at the expense of my health

--- day-10/solution.js
+++ day-10/solution.js
@@ -6,14 +6,49 @@ const {
 } = require("../helpers/readAndTransformInputFile");
 
 const inputFilePath = path.resolve(__dirname, "./input.txt");
-const data = readAndTransformInputFile(inputFilePath);
+let adapters = readAndTransformInputFile(inputFilePath)
+  .map(Number)
+  .sort((a, b) => a - b);
+// Add the outlet and device to the chain
+adapters = [0, ...adapters, adapters[adapters.length - 1] + 3];
+
+const adapterChainReducer = (adapterChainCounts, currentAdapter) => {
+  const adapterChainCountReducer = (adapterChainCount, nextAdaptor) =>
+    Object.assign({}, adapterChainCount, {
+      [nextAdaptor]:
+        (adapterChainCounts[nextAdaptor] || 0) +
+        (adapterChainCounts[currentAdapter] || 1),
+    });
+
+  const nextAdaptors = adapters.filter(
+    (adapterOption) =>
+      adapterOption !== currentAdapter &&
+      adapterOption <= currentAdapter + 3 &&
+      adapterOption > currentAdapter
+  );
+
+  const updatedChainCounts = nextAdaptors.reduce(adapterChainCountReducer, {});
+
+  return Object.assign({}, adapterChainCounts, updatedChainCounts);
+};
 
 function part1() {
-  return "NOT_IMPLEMENTED";
+  let diffs = {
+    1: 1,
+    2: 0,
+    3: 1,
+  };
+  for (let i = 0; i < adapters.length; i += 1) {
+    const difference = adapters[i + 1] - adapters[i];
+    diffs[difference] += 1;
+  }
+  return diffs[1] * diffs[3];
 }
 
 function part2() {
-  return "NOT_IMPLEMENTED";
+  return adapters.reduce(adapterChainReducer, { 0: 1 })[
+    adapters[adapters.length - 1]
+  ];
 }
 
 module.exports = { part1, part2 };
