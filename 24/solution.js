e18a01afd11989e77ffd8d0484fb99e00e6a040d
holdercp <c_hold@icloud.com>
2021-01-07 13:47:34 -0600

    Complete day 24, part 1

--- 24/solution.js
+++ 24/solution.js
@@ -6,10 +6,55 @@ const {
 } = require("../helpers/readAndTransformInputFile");
 
 const inputFilePath = path.resolve(__dirname, "./input.txt");
-const data = readAndTransformInputFile(inputFilePath);
+
+const getDirections = (line) => {
+  let directions = [];
+  for (let i = 0; i < line.length; i += 1) {
+    const char = line[i];
+    const nextChar = line[i + 1];
+
+    if (char === "e" || char === "w") {
+      directions.push(char);
+    } else {
+      directions.push(`${char}${nextChar}`);
+      i += 1;
+    }
+  }
+  return directions;
+};
+
+const list = readAndTransformInputFile(inputFilePath).map(getDirections);
+
+const move = (direction, [x, y]) => {
+  if (direction === "e") return [x + 2, y];
+  if (direction === "w") return [x - 2, y];
+
+  if (direction === "ne") return [x + 1, y - 1];
+  if (direction === "nw") return [x - 1, y - 1];
+
+  if (direction === "se") return [x + 1, y + 1];
+  if (direction === "sw") return [x - 1, y + 1];
+
+  throw new Error("Invalid direction passed to move");
+};
+
+const followDirections = (position, direction) => move(direction, position);
 
 function part1() {
-  return "NOT_IMPLEMENTED";
+  const blackTiles = new Set();
+
+  list.forEach((directions) => {
+    const position = directions.reduce(followDirections, [0, 0]);
+    const tile = position.join(".");
+
+    if (blackTiles.has(tile)) {
+      blackTiles.delete(tile);
+    } else {
+      blackTiles.add(tile);
+    }
+  });
+
+  return blackTiles.size;
 }
 
 function part2() {
