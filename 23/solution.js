fa3b647b92e2b32a6bdeb1c195b55cf163ec46f8
holdercp <c_hold@icloud.com>
2021-01-13 16:45:26 -0600

    Day 23, part 2 complete

--- 23/solution.js
+++ 23/solution.js
@@ -5,63 +5,116 @@ const {
   readAndTransformInputFile,
 } = require("../helpers/readAndTransformInputFile");
 
-const MOVES = 100;
-
 const inputFilePath = path.resolve(__dirname, "./input.txt");
 const data = readAndTransformInputFile(inputFilePath)[0].split("").map(Number);
 
-function pickup(cups, currentIndex) {
-  let picked = [];
-  for (let i = 1; i <= 3; i += 1) {
-    const position = (currentIndex + i) % cups.length;
-    picked.push(cups[position]);
+const getCircularIndex = (current, offset, arr) =>
+  (current + offset) % arr.length;
+
+function pickup(cups, current) {
+  const picked = [];
+  let pick = cups.get(current);
+  for (let i = 0; i < 3; i += 1) {
+    picked.push(pick);
+    let next = cups.get(pick);
+    cups.delete(pick);
+    pick = next;
   }
-
-  const remaining = cups.filter((cup) => !picked.includes(cup));
-
-  return { picked, remaining };
+  cups.set(current, pick);
+  return picked;
 }
 
-function chooseDestination(remaining, currentValue) {
-  const candidates = [...remaining].sort((a, b) => b - a);
-  const currentIndex = candidates.indexOf(currentValue);
-  const value = candidates[(currentIndex + 1) % candidates.length];
-  const index = remaining.indexOf(value);
+function chooseDestination(cups, current, picked) {
+  let destination = null;
+  let candidate = current - 1;
+  while (!destination) {
+    if (candidate === 0) {
+      candidate = cups.size + picked.length;
+    } else if (picked.includes(candidate)) {
+      candidate -= 1;
+    } else {
+      destination = candidate;
+    }
+  }
+  return destination;
+}
 
-  return { index, value };
+function insert(cups, picked, destination) {
+  const end = cups.get(destination);
+  let current = destination;
+  picked.forEach((cup) => {
+    cups.set(current, cup);
+    current = cup;
+  });
+  cups.set(current, end);
 }
 
 function move(cups, current) {
-  const { picked, remaining } = pickup(cups, current.index);
-  const destination = chooseDestination(remaining, current.value);
-  const lower = remaining.slice(0, destination.index + 1);
-  const upper = remaining.slice(destination.index + 1);
-  return [...lower, ...picked, ...upper];
+  const picked = pickup(cups, current);
+  const destination = chooseDestination(cups, current, picked);
+  insert(cups, picked, destination);
 }
 
 function getOrder(cups) {
-  const start = cups.indexOf(1);
+  let next = 1;
   let order = "";
-  for (let i = 1; i < cups.length; i += 1) {
-    order += cups[(start + i) % cups.length];
+
+  for (let i = 1; i < cups.size; i += 1) {
+    const current = cups.get(next);
+    order += String(current);
+    next = current;
   }
   return order;
 }
 
+function getNextTwo(cups) {
+  const one = cups.get(1);
+  const two = cups.get(one);
+  return [one, two];
+}
+
+function getExtraCups(cups, moves) {
+  const max = Math.max(...cups);
+  const length = moves - max;
+
+  let arr = [];
+  for (let i = 1; i <= length; i += 1) {
+    arr.push(max + i);
+  }
+  return arr;
+}
+
+function init(cups) {
+  const cupsMap = new Map();
+  for (let current = 0; current < cups.length; current += 1) {
+    const next = getCircularIndex(current, 1, cups);
+    cupsMap.set(cups[current], cups[next]);
+  }
+  return cupsMap;
+}
+
 function part1() {
-  let cups = [...data];
-  let current = { index: 0, value: cups[0] };
-  for (let i = 1; i <= MOVES; i += 1) {
-    cups = move(cups, current);
-    const nextIndex = (cups.indexOf(current.value) + 1) % cups.length;
-    current = { index: nextIndex, value: cups[nextIndex] };
+  const moves = 100;
+  let cups = init(data);
+  let current = cups.keys().next().value;
+  for (let i = 1; i <= moves; i += 1) {
+    move(cups, current);
+    current = cups.get(current);
   }
 
   return getOrder(cups);
 }
 
 function part2() {
-  return "NOT_IMPLEMENTED";
+  const moves = 10000000;
+  let cups = init([...data, ...getExtraCups(data, 1000000)]);
+  let current = cups.keys().next().value;
+  for (let i = 1; i <= moves; i += 1) {
+    move(cups, current);
+    current = cups.get(current);
+  }
+  const [one, two] = getNextTwo(cups);
+  return one * two;
 }
 
 module.exports = { part1, part2 };
