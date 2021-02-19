ec2e81abbeb4aff4a9c0e9d30e628d9ce166141c
holdercp <c_hold@icloud.com>
2021-02-18 22:54:09 -0600

    Complete day 20, pt 2... which I hated

--- 20/solution.js
+++ 20/solution.js
@@ -46,132 +46,52 @@ const createTiles = (tile) => {
   return new Tile(id, edges, data);
 };
 
+const search = (
+  tile,
+  candidates,
+  matched,
+  tiles,
+  cb,
+  [tileEdge, candidateEdge]
+) => {
+  candidates.forEach((candidate) => {
+    if (!matched.includes(candidate.id)) {
+      let match = false;
+      let edgesChecked = 0;
+      while (!match && edgesChecked <= 8) {
+        if (tile[tileEdge] === candidate[candidateEdge]) {
+          match = true;
+          matched.push(candidate.id);
+        } else {
+          candidate.rotate();
+          if (edgesChecked === 4) {
+            candidate.reset();
+            candidate.flip();
+          }
+          edgesChecked += 1;
+        }
+      }
+
+      if (match) {
+        cb(
+          candidate,
+          candidates.filter((other) => other.id !== candidate.id),
+          matched,
+          tiles
+        );
+      } else {
+        candidate.reset();
+      }
+    }
+  });
+};
+
 const searchForNeighbors = (tile, candidates, matched, tiles) => {
-  // Search for top match
-  candidates.forEach((candidate) => {
-    if (!matched.includes(candidate.id)) {
-      let match = false;
-      let edgesChecked = 0;
-      while (!match && edgesChecked <= 8) {
-        if (tile.top === candidate.bottom) {
-          match = true;
-          matched.push(candidate.id);
-        } else {
-          candidate.rotate();
-          if (edgesChecked === 4) {
-            candidate.reset();
-            candidate.flip();
-          }
-          edgesChecked += 1;
-        }
-      }
-
-      if (match) {
-        searchForNeighbors(
-          candidate,
-          candidates.filter((other) => other.id !== candidate.id),
-          matched,
-          tiles
-        );
-      } else {
-        candidate.reset();
-      }
-    }
-  });
-
-  // Search for right match
-  candidates.forEach((candidate) => {
-    if (!matched.includes(candidate.id)) {
-      let match = false;
-      let edgesChecked = 0;
-      while (!match && edgesChecked <= 8) {
-        if (tile.right === candidate.left) {
-          match = true;
-          matched.push(candidate.id);
-        } else {
-          candidate.rotate();
-          if (edgesChecked === 4) {
-            candidate.reset();
-            candidate.flip();
-          }
-          edgesChecked += 1;
-        }
-      }
-
-      if (match) {
-        searchForNeighbors(
-          candidate,
-          candidates.filter((other) => other.id !== candidate.id),
-          matched,
-          tiles
-        );
-      } else {
-        candidate.reset();
-      }
-    }
-  });
-
-  candidates.forEach((candidate) => {
-    if (!matched.includes(candidate.id)) {
-      let match = false;
-      let edgesChecked = 0;
-      while (!match && edgesChecked <= 8) {
-        if (tile.bottom === candidate.top) {
-          match = true;
-          matched.push(candidate.id);
-        } else {
-          candidate.rotate();
-          if (edgesChecked === 4) {
-            candidate.reset();
-            candidate.flip();
-          }
-          edgesChecked += 1;
-        }
-      }
-
-      if (match) {
-        searchForNeighbors(
-          candidate,
-          candidates.filter((other) => other.id !== candidate.id),
-          matched,
-          tiles
-        );
-      } else {
-        candidate.reset();
-      }
-    }
-  });
-
-  candidates.forEach((candidate) => {
-    if (!matched.includes(candidate.id)) {
-      let match = false;
-      let edgesChecked = 0;
-      while (!match && edgesChecked <= 8) {
-        if (tile.left === candidate.right) {
-          match = true;
-          matched.push(candidate.id);
-        } else {
-          candidate.rotate();
-          if (edgesChecked === 4) {
-            candidate.reset();
-            candidate.flip();
-          }
-          edgesChecked += 1;
-        }
-      }
-
-      if (match) {
-        searchForNeighbors(
-          candidate,
-          candidates.filter((other) => other.id !== candidate.id),
-          matched,
-          tiles
-        );
-      } else {
-        candidate.reset();
-      }
-    }
-  });
+  const common = [tile, candidates, matched, tiles, searchForNeighbors];
+  search(...common, ["top", "bottom"]);
+  search(...common, ["right", "left"]);
+  search(...common, ["bottom", "top"]);
+  search(...common, ["left", "right"]);
 
   tiles
     .filter((tile1) => tile1.id !== tile.id)
@@ -224,6 +144,59 @@ const traverseTiles = (keyTile) => {
   return data;
 };
 
+const rotateData = (data) => {
+  const dataReversed = [...data].reverse();
+  let rotatedData = [];
+  data.forEach((l, index) => {
+    dataReversed.forEach((line) => {
+      rotatedData[index] = (rotatedData[index] || "") + line[index];
+    });
+  });
+  return rotatedData;
+};
+
+const flipData = (data) =>
+  data.map((line) => line.split("").reverse().join(""));
+
+const isMonsterFound = ([x, y], data) =>
+  data[y][x] === "#" &&
+  data[y + 1][x + 1] === "#" &&
+  data[y + 1][x + 4] === "#" &&
+  data[y][x + 5] === "#" &&
+  data[y][x + 6] === "#" &&
+  data[y + 1][x + 7] === "#" &&
+  data[y + 1][x + 10] === "#" &&
+  data[y][x + 11] === "#" &&
+  data[y][x + 12] === "#" &&
+  data[y + 1][x + 13] === "#" &&
+  data[y + 1][x + 16] === "#" &&
+  data[y][x + 17] === "#" &&
+  data[y][x + 18] === "#" &&
+  data[y - 1][x + 18] === "#" &&
+  data[y][x + 19] === "#";
+
+const countSeaMonsters = (data) => {
+  const monsterHeight = 3;
+  const monsterWidth = 20;
+
+  let yIndex = 1;
+  let count = 0;
+
+  while (yIndex + monsterHeight <= data.length) {
+    let xIndex = 0;
+    while (xIndex + monsterWidth <= data.length) {
+      const found = isMonsterFound([xIndex, yIndex], data);
+      if (found) {
+        count += 1;
+      }
+      xIndex += 1;
+    }
+    yIndex += 1;
+  }
+
+  return count;
+};
+
 function part1() {
   const tiles = rawTiles.map(createTiles);
   arrangeTiles(tiles);
@@ -241,8 +214,30 @@ function part2() {
       !tile.neighbors.has("left") &&
       !tile.neighbors.has("top")
   );
-  const arrangedData = traverseTiles(keyTile);
-  return arrangedData;
+
+  let arrangedData = traverseTiles(keyTile);
+  let count = 0;
+  let timesSearched = 0;
+
+  while (count === 0 && timesSearched <= 8) {
+    count += countSeaMonsters(arrangedData);
+    timesSearched += 1;
+    arrangedData = rotateData(arrangedData);
+
+    if (timesSearched === 4) {
+      arrangedData = rotateData(arrangedData);
+      arrangedData = flipData(arrangedData);
+    }
+  }
+
+  return (
+    arrangedData.reduce(
+      (total, line) =>
+        total + line.split("").filter((point) => point === "#").length,
+      0
+    ) -
+    count * 15
+  );
 }
 
 module.exports = { part1, part2 };
