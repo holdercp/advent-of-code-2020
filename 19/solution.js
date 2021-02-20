51b27264e5ebf3ee77dffff49e7a8e9f229f2ae6
holdercp <c_hold@icloud.com>
2021-02-20 14:51:22 -0600

    Day 19, pt 2 done

--- 19/solution.js
+++ 19/solution.js
@@ -35,6 +35,32 @@ const resolve = (ruleNumber, rulesMap) => {
   return `(${resolvedValues})`;
 };
 
+const resolveWithLoop = (ruleNumber, rulesMap, loopTracker, depth = 0) => {
+  const values = rulesMap.get(ruleNumber);
+  let nextDepth = depth;
+
+  if (typeof values === "string") {
+    return values;
+  }
+
+  const resolvedValues = values.reduce((resolved, value) => {
+    if (value === "|") {
+      return resolved + value;
+    }
+
+    if (value === loopTracker.loopedRule) {
+      if (depth === loopTracker.max) {
+        return resolved + loopTracker.loopedResolution;
+      }
+      nextDepth += 1;
+    }
+
+    return resolved + resolveWithLoop(value, rulesMap, loopTracker, nextDepth);
+  }, "");
+
+  return `(${resolvedValues})`;
+};
+
 function part1() {
   const rulesMap = new Map(rules.map(createMap));
   const matcher = resolve("0", rulesMap);
@@ -52,6 +78,24 @@ function part2() {
   const rulesMap = new Map(rules.map(createMap));
   rulesMap.set("8", ["42", "|", "42", "8"]);
   rulesMap.set("11", ["42", "31", "|", "42", "11", "31"]);
+
+  // Pre-resolve the rules 8 and 11 use
+  rulesMap.set("42", resolve("42", rulesMap));
+  rulesMap.set("31", resolve("31", rulesMap));
+
+  // Pre-resolve 8 and 11 with enough loops to account for our input
+  const loopTrackerFor8 = {
+    loopedRule: "8",
+    resolution: rulesMap.get("42"),
+    max: 10,
+  };
+  const loopTrackerFor11 = {
+    loopedRule: "11",
+    resolution: `${rulesMap.get("42")}${rulesMap.get("31")}`,
+    max: 10,
+  };
+  rulesMap.set("8", resolveWithLoop("8", rulesMap, loopTrackerFor8));
+  rulesMap.set("11", resolveWithLoop("11", rulesMap, loopTrackerFor11));
 
   const matcher = resolve("0", rulesMap);
   const pattern = new RegExp(matcher, "g");
